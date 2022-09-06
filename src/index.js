e5901b2e7aa72662a682be284a25e41d596fefe2
Omar Shehata <omar.sameh.shehata@gmail.com>
2022-09-05 20:31:59 -0400

    Correctly use /public for static data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import HelperClass from "./HelperClass.js";
 import helperFunction from "./helperFunction.js";
 import { anotherFunction } from "./helperFunction.js";
-import imageUrl from '../static/test.jpeg?url';
+// import imageUrl from '../public/test.jpeg?url';
 // To load a raw text file, use ?raw instead of ?url
 
 const helper = new HelperClass();
@@ -13,6 +13,7 @@ anotherFunction();
 
 // Example loading static file from JS
 async function fetchImage() {
+  const imageUrl = '/test.jpeg'
   const request = await fetch(imageUrl);
   const response = await request.blob();
   console.log(response);
